50b2c1e0486a0aed905a060fd2455301446c3845
Hrvoje <hrvoje.ljubic@fpmoz.sum.ba>
2021-04-22 11:26:07 +0200

    Initial commit

---
+++ src/views/News.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Novosti</h1>
+
+    <moja-kartica naslov="Prva novost" tekst="U VueJS sve gledamo kao komponetu." />
+    <moja-kartica naslov="Druga novost" tekst="Ovdje smo iskoritili opet MojuKarticu." />
+    <moja-kartica naslov="Treća novost" tekst="I to je to. :)" />
+  </div>
+</template>
+
+<script>
+import MojaKartica from '../components/MojaKartica.vue'
+
+export default {
+  name: 'Profile',
+  components: {
+    MojaKartica
+  }
+}
+</script>
